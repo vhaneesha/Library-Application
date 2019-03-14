9512295c2eedd3457fb1c4764f1ed0b865c921ca
Haneesha Vallamsetti <35718636+vhaneesha@users.noreply.github.com>
2019-03-14 13:19:06 -0700

    Update EditItems.java

--- src/asst/servlet/EditItems.java
+++ src/asst/servlet/EditItems.java
@@ -118,7 +118,7 @@ public class EditItems extends HttpServlet {
         {
             String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu27";
             String username = "cs3220stu27";
-            String password = "VXlx!72S";
+            String password = "**********";
             
         		//Integer id = Integer.valueOf(request.getParameter( "id" ));
         		
