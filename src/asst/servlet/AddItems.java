37ee2d51498233506dbce6f9c68fc325126a4d33
Haneesha Vallamsetti <35718636+vhaneesha@users.noreply.github.com>
2019-03-14 13:16:43 -0700

    Update AddItems.java

--- src/asst/servlet/AddItems.java
+++ src/asst/servlet/AddItems.java
@@ -105,7 +105,7 @@ public void init(ServletConfig config) throws ServletException {
 		        {
 		            String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu27";
 		            String username = "cs3220stu27";
-		            String password = "VXlx!72S";
+		            String password = "********";
 
 		            c = DriverManager.getConnection( url, username, password );
 		            while(copies>0) {
