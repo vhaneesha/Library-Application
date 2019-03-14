5441c1847c2a9dbdd669fb8d9acbd46305134f2a
Haneesha Vallamsetti <35718636+vhaneesha@users.noreply.github.com>
2019-03-14 13:20:17 -0700

    Update Type.java

--- src/asst/servlet/Type.java
+++ src/asst/servlet/Type.java
@@ -47,7 +47,7 @@ public void init(ServletConfig config) throws ServletException {
         {
             String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu27?useSSL=false";
             String username = "cs3220stu27";
-            String password = "VXlx!72S";
+            String password = "**********";
           
         		
             c = DriverManager.getConnection( url, username, password );
